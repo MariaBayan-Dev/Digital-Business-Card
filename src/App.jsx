c44235aa1fc96d9fd37f44fe22f4a5d44d252154
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-25 09:40:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import ProfileHeader from './components/ProfileHeader'
+import About from './components/About'
+import SocialFooter from './components/SocialFooter'
+
+export default function App(){
+  return(
+    <>
+    <article>
+        <ProfileHeader/>
+        <About/>
+        <SocialFooter />
+    </article>
+    </>
+  )
+}
+
+
+
