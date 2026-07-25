2178de6c69680b58d8ab844ba9c008c2fc62f113
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-25 19:29:11 +0200

    Header css iniciado.

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,12 +1,22 @@
 import photoProfile from '../assets/photo-profile.png'
+import { Mail, Link } from 'lucide-react'
 
 export default function ProfileHeader(){
     return(
         <header>
-            <img src={photoProfile} alt="Imagen de perfil" />
-            <h1>María Bayán Escobar</h1>
-            <span>Frontend Developer Junior</span>
-            <p>HTML, CSS, JavaScript, React, Vite</p>
+            <img src={photoProfile} alt="Imagen de perfil de María Bayán" />
+            <h1 className="header__title">María Bayán Escobar</h1>
+            <p className="header__subtitle">Frontend Developer Junior</p>
+            <p className="header__tech">HTML, CSS, JavaScript, React, Vite</p>
+
+            <div className="contact-links">
+                <a href="mailto:mariabayanescobar@gmail.com" rel="noopener noreferrer" target="_blank">
+                    <Mail />
+                </a>
+                <a href="https://mariabayan.netlify.app/" rel="noopener noreferrer" target="_blank">
+                    <Link />
+                </a>
+            </div>
         </header>
     )
 }
