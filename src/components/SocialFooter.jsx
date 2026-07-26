631db2d38dfed8010bfaeaa24f089625b835b21d
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-27 00:27:10 +0200

    CSS Footer.

--- src/components/SocialFooter.jsx
+++ src/components/SocialFooter.jsx
@@ -1,5 +1,17 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+
 export default function SocialFooter(){
     return(
-        <p>Aqui va el footer</p>
+        <footer>
+            <div className="footer__social">
+                <a href="">
+                    <FaGithub size={28}/>
+                </a>
+
+                <a href="">
+                    <FaLinkedin size={28}/>
+                </a>
+            </div>
+        </footer>
     )
 }
