d68acfa4b38d23f4ebbf94d3dda79759e6a96f2f
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-26 18:06:01 +0200

    CSS About Section iniciado.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,21 @@
 export default function AboutSection(){
     return(
-        <p>Aqui va la seccion.</p>
+        <section>
+            <div className="section__about">
+                <h2 className="section__title">Sobre mí</h2>
+                <p className="section__text">
+                    Autodidacta, resolutiva y con capacidad de aprendizaje rápido, ideal para adaptarme 
+                    a nuevos equipos y retos.
+                </p>
+            </div>
+
+            <div className="section__interests">
+                <h2 className="section__title">Intereses</h2>
+                <p className="section__text">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Quo, officia?
+                </p>
+            </div>
+        </section>
     )
 }
